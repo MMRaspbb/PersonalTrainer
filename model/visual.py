636d6331e469ec695d7b4a9e69433d99692cc6a8
Wiktor Bukowski <vecnine@Wiktors-MacBook-Pro.local>
2026-04-18 15:50:51 +0200

    lol

--- model/visual.py
+++ model/visual.py
@@ -1,3 +1,33 @@
-from mediapipe.tasks.python import vision
+import mediapipe as mp
 
-vision()
+model_path = '/Users/vecnine/Desktop/programowanie/python projects/PersonalTrainer/model/tasks/pose_landmarker_full.task'
+
+BaseOptions = mp.tasks.BaseOptions
+PoseLandmarker = mp.tasks.vision.PoseLandmarker
+PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
+PoseLandmarkerResult = mp.tasks.vision.PoseLandmarkerResult
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+# Create a pose landmarker instance with the live stream mode:
+def print_result(result: PoseLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+    print('pose landmarker result: {}'.format(result))
+
+options = PoseLandmarkerOptions(
+    base_options=BaseOptions(model_asset_path=model_path),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=print_result)
+
+# Use OpenCV’s VideoCapture to start capturing from the webcam.
+
+# Create a loop to read the latest frame from the camera using VideoCapture#read()
+
+# Convert the frame received from OpenCV to a MediaPipe’s Image object.
+mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=numpy_frame_from_opencv)
+
+with PoseLandmarker.create_from_options(options) as landmarker:
+  # The landmarker is initialized. Use it here.
+  # ...
+
+
+
+landmarker.detect_async(mp_image, frame_timestamp_ms)
