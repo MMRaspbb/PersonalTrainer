f59fdc2a9691bd90210fedaa130574978d728806
MMRaspbb <maciek.malinowski.poczta@gmail.com>
2026-04-18 14:13:01 +0200

    Inicialized Spring Boot backend and ReactJS frontend

---
+++ trainer/src/test/java/com/hack/carpatia/trainer/TrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hack.carpatia.trainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
