3d851d5243dafb59e781e5fb727edc39b74fc38f
jar <reavo28@gmail.com>
2026-04-18 18:22:15 +0200

    Revert "Dodanie feedback i testow do dzialania"
    This reverts commit feb18c0535a668b0c936a79a95915986d96e7563.
    
    with '#' will be ignored, and an empty message aborts the commit.

--- trainer_backend/core/exercise_logic.py
+++ trainer_backend/core/exercise_logic.py
@@ -1,9 +1,9 @@
-from .feedback import FeedbackManager
 from .math_engine import calculate_angle, get_distance
 
 
+
 class SquatCounter:
-    """Licznik przysiadów z dynamicznym feedbackiem."""
+    """Licznik przysiadów z kontrolą głębokości (90 stopni)."""
 
     def __init__(self):
         self.counter = 0
@@ -11,69 +11,48 @@ class SquatCounter:
         self.threshold_down = 90.0
         self.threshold_up = 160.0
 
-        self.fm = FeedbackManager()
-        self.feedback = self.fm.get("squat", "start")
-
     def update(self, hip, knee, ankle):
         angle = calculate_angle(hip, knee, ankle)
 
-        if angle > self.threshold_up:
-            if self.stage == "down":
-                self.counter += 1
-                self.feedback = self.fm.get("squat", "good_rep")
-            self.stage = "up"
-
-        elif angle < self.threshold_down:
-            if self.stage == "up":
-                self.feedback = self.fm.get("squat", "go_up")
+        if angle < self.threshold_down:
             self.stage = "down"
 
-        else:
-            if self.stage == "up":
-                self.feedback = self.fm.get("squat", "go_lower")
-            elif self.stage == "down":
-                self.feedback = self.fm.get("squat", "extend_fully")
+        if angle > self.threshold_up and self.stage == "down":
+            self.counter += 1
+            self.stage = "up"
 
-        return self.counter, self.stage, angle, self.feedback
+        return self.counter, self.stage, angle
 
 
 class PushupCounter:
-    """Korektor pompek ze sprawdzaniem osiowości pleców i dynamicznym feedbackiem."""
+    """Korektor pompek ze sprawdzaniem osiowości kręgosłupa."""
 
     def __init__(self):
         self.counter = 0
         self.stage = "up"
-
-        self.fm = FeedbackManager()
-        self.feedback = self.fm.get("pushup", "start")
+        self.feedback = ""
 
     def check_alignment(self, shoulder, hip, ankle):
-        """Sprawdza czy plecy są w linii prostej (akceptowalny kąt 160-200 stopni)."""
+        """Sprawdza, czy plecy są w linii prostej."""
         body_angle = calculate_angle(shoulder, hip, ankle)
         return 160 <= body_angle <= 200, body_angle
 
     def update(self, shoulder, elbow, wrist, hip, ankle):
         is_straight, body_angle = self.check_alignment(shoulder, hip, ankle)
-
         arm_angle = calculate_angle(shoulder, elbow, wrist)
 
         if arm_angle > 160:
             if self.stage == "down":
                 if is_straight:
                     self.counter += 1
-                    self.feedback = self.fm.get("pushup", "good_rep")
+                    self.feedback = "Świetna technika!"
                 else:
-                    self.feedback = self.fm.get("pushup", "rep_bad_form")
+                    self.feedback = "Pompka wykonana, ale popraw BIODRA!"
             self.stage = "up"
-
         elif arm_angle < 90:
             self.stage = "down"
-            if not is_straight:
-                self.feedback = self.fm.get("pushup", "bad_back")
-            else:
-                self.feedback = self.fm.get("pushup", "go_up")
 
-        return self.counter, self.stage, arm_angle, self.feedback
+        return self.counter, self.stage, self.feedback, arm_angle
 
 
 class CalibrationManager:
